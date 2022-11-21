36ea0dd0ebc544e363c6fa698af915af2c9cb931
CrazyCat <loxtox74@gmail.com>
2022-11-21 13:41:57 +0100

    Vytvoření základních složek

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
