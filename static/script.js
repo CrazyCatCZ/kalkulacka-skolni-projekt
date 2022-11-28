f9ff5a19d4c3e95d3957b14c6481b0af13806155
CrazyCat <loxtox74@gmail.com>
2022-11-28 09:35:08 +0100

    Vyhodnocení inputu

--- static/script.js
+++ static/script.js
@@ -1,4 +1,7 @@
 const input = document.getElementById("input")
+const enterButton = document.getElementById("enterButton")
+const clearButton = document.getElementById("clearButton")
+const clearButton2 = document.getElementById("clearButton2")
 const buttons = document.querySelector("#calculator").querySelectorAll("button")
 
 const canPress = [
@@ -10,6 +13,11 @@ const canPressSign = [
     "+", "-", "*", "/"
 ]
 
+const notShowableButtons = [
+    enterButton, clearButton, clearButton2
+]
+
+
 let characterOnIndex = (index) => {
   let value = input.value
   return value[value.length - index]
@@ -18,14 +26,32 @@ let characterOnIndex = (index) => {
 // Display input on screen
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        let lastIndex = button.getAttribute('value')
-        let penultimateIndex = characterOnIndex(1)
+        const buttonCanBeShown = !notShowableButtons.includes(button)
 
-        if (canPressSign.includes(lastIndex) & canPressSign.includes(penultimateIndex)) {
-            console.log('last index is sign')
+        if (buttonCanBeShown) {
+            let lastIndex = button.getAttribute('value')
+            let penultimateIndex = characterOnIndex(1)
+    
+            if (canPressSign.includes(lastIndex) & canPressSign.includes(penultimateIndex)) {
+                console.log('last index is sign')
+            }
+            else {
+                input.value += lastIndex
+            }
         }
-        else {
-            input.value += lastIndex
-        }
+
     })
 })
+
+// Checks user input or evaluate
+enterButton.addEventListener('click', (event) => {
+    if (input.value == '') {
+        input.placeholder = 'Enter something'
+    }
+
+    else {
+        let result = eval(input.value)
+        input.value = result
+    }
+})
+
