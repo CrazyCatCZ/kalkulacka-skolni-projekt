1b4efd11683a1004eca5b7a0ca9aa4b9b5616487
CrazyCat <loxtox74@gmail.com>
2022-12-12 09:04:38 +0100

    Show characters on display

--- static/script.js
+++ static/script.js
@@ -12,23 +12,27 @@ $(document).ready(() => {
         $("#enterButton"), $("clearButton"), $("clearButton2"),
         $("deleteButton"), $("revereverseSignButton")
     ]
-
+    
+    let characterOnIndex = (index) => {
+      let value = $("#input").val()
+      return value[value.length - index]
+    }
+    
     // Display input on screen
-    $("#calculator > button").bind("click", function() {
-        console.log("test")
-            const buttonCanBeShown = !notShowableButtons.includes(button)
+    $(".row > button").click(function() {
+        const button = $(this);
+        const buttonCanBeShown = !notShowableButtons.includes(button)
 
-            if (buttonCanBeShown) {
-                let lastIndex = button.getAttribute('value')
-                let penultimateIndex = characterOnIndex(1)
+        if (buttonCanBeShown) {
+            let lastIndex = button.val()
+            let penultimateIndex = characterOnIndex(1)
         
-                if (canPressSign.includes(lastIndex) & canPressSign.includes(penultimateIndex)) {
+            if (canPressSign.includes(lastIndex) & canPressSign.includes(penultimateIndex)) {
                     console.log('last index is sign')
-                }
-                else {
-                    $("#input").val(lastIndex)
-                }
             }
-
-        })
+            else {
+                $("#input").val(lastIndex)
+            }
+        }
     })
+})
