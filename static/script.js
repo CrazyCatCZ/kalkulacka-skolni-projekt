1dcd927218a8c30dea6cd93c072a744e80b9d772
CrazyCat <loxtox74@gmail.com>
2022-12-12 09:33:05 +0100

    Vyhodncení výsledku

--- static/script.js
+++ static/script.js
@@ -17,24 +17,52 @@ $(document).ready(() => {
       let value = $("#input").val()
       return value[value.length - index]
     }
+
+    const clearDisplay = () => {
+        $("#input").val("")
+    }
+
+    const deleteKey = () => {
+    let value = $("#input").val()
+        $("#input").val(value.substring(0, value.length - 1))
+    }
+
+    const reverseSign = () => {
+        $("#input").val($("input").val() * -1)
+    }
     
     // Display input on screen
     $(".row > button").click(function() {
+        console.log("test")
         const button = $(this);
         const buttonCanBeShown = (jQuery.inArray(button.val(), notShowableButtons)) == -1
-
-        console.log(notShowableButtons, button.val())
 
         if (buttonCanBeShown) {
             let lastIndex = button.val()
             let penultimateIndex = characterOnIndex(1)
         
             if (canPressSign.includes(lastIndex) & canPressSign.includes(penultimateIndex)) {
-                    console.log('last index is sign')
+                console.log('last index is sign')
             }
             else {
-                $("#input").val(lastIndex)
+                console.log("test")
+                let temp = $("#input").val();
+                temp += lastIndex;
+                $("#input").val(temp);
             }
         }
+    })
+    // Checks user input or evaluate
+    $("#enterButton").click(function () {
+        const input = $(this);
+        const lastCharacter = input.val()
+        const lastCharacterIsSign = canPressSign.includes(lastCharacter)
+
+        if ($("#input").val() == '') {
+            $("#input").val("Enter something...")
+        }
+
+        let result = eval($("#input").val())
+        $("input").val(result)
     })
 })
