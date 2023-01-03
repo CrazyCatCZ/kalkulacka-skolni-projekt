8ac3d9ef31db994448579c9eeff8340510ae4bfd
Jakub Plecháč <loxtox74@gmail.com>
2023-01-03 22:04:22 +0100

    Posílaní dotazů na server

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ import express from "express";
 
 const PORT = 5000;
 const app = express();
+const router = express.Router();
 
 app.use(express.static("static"));
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  next();
+});
+
+// Statistika tlačítek
+router.post("/api", (req, res) => {
+  res.json({ message: "test" });
+});
+
+app.use(router);
--- static/statistic.js
+++ static/statistic.js
@@ -1,7 +1,19 @@
 const allButtons = document.querySelectorAll("button");
+const localHost = "http://localhost:5000";
 
 allButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    console.log(button.value);
+  button.addEventListener("click", async () => {
+    const clickedButton = button.textContent;
+
+    await fetch(`${localHost}/api`, {
+      method: "POST",
+      body: JSON.stringify(clickedButton),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
   });
 });
