f4947f8f714efc987186ab39af1638feb659e2dc
Jakub Plecháč <loxtox74@gmail.com>
2023-01-03 19:49:59 +0100

    Oprava vyhodnocení prázdného inputu

--- static/script.js
+++ static/script.js
@@ -1,133 +1,150 @@
-
 $(document).ready(() => {
-    const SQUARE = 2
+  const SQUARE = 2;
 
-    const canPress = [
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-        "+", "-", "*", "/",
-    ]
-    const canPressSign = [
-        "+", "-", "*", "/"
-    ]
-    
-    const notShowableButtons = [
-        $("#enterButton").val(), $("#clearButton").val(), $("#clearButton2").val(),
-        $("#deleteButton").val(), $("#reverseSignButton").val(), $("#inverseButton").val(),
-        $("#percentButton").val(), $("#squareButton").val(), $("#squareRootButton").val()
-    ]
-    
-    let characterOnIndex = (index) => {
-      let value = $("#input").val()
-      return value[value.length - index]
+  const canPress = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "+",
+    "-",
+    "*",
+    "/",
+  ];
+  const canPressSign = ["+", "-", "*", "/"];
+
+  const notShowableButtons = [
+    $("#enterButton").val(),
+    $("#clearButton").val(),
+    $("#clearButton2").val(),
+    $("#deleteButton").val(),
+    $("#reverseSignButton").val(),
+    $("#inverseButton").val(),
+    $("#percentButton").val(),
+    $("#squareButton").val(),
+    $("#squareRootButton").val(),
+  ];
+
+  let characterOnIndex = (index) => {
+    let value = $("#input").val();
+    return value[value.length - index];
+  };
+
+  const clearDisplay = () => {
+    $("#input").val("");
+  };
+
+  const deleteKey = () => {
+    let value = $("#input").val();
+    $("#input").val(value.substring(0, value.length - 1));
+  };
+
+  const reverseSign = () => {
+    let temp = $("#input").val();
+    temp = temp * -1;
+
+    $("#input").val(temp);
+  };
+
+  const square = () => {
+    if ($("#input").val() !== "") {
+      let temp = $("#input").val();
+      temp = eval(temp);
+      temp = Math.pow(temp, SQUARE);
+
+      $("#input").val(temp);
     }
+  };
 
-    const clearDisplay = () => {
-        $("#input").val("")
+  const squareRoot = () => {
+    if ($("#input").val() !== "") {
+      let temp = $("#input").val();
+      temp = eval(input.value);
+      temp = Math.sqrt(temp, SQUARE);
+
+      $("#input").val(temp);
     }
+  };
 
-    const deleteKey = () => {
-    let value = $("#input").val()
-        $("#input").val(value.substring(0, value.length - 1))
+  const reciprocate = () => {
+    if ($("#input").val() !== "") {
+      let temp = $("#input").val();
+      temp = eval(input.value);
+      temp = 1 / temp;
+
+      $("#input").val(temp);
     }
+  };
 
-    const reverseSign = () => {
-        let temp = $("#input").val();
-        temp = temp * -1;
+  const calculatePercetage = () => {
+    alert("Pro toto tlačítka nebyla do zatím přidaná funkcionalita");
+  };
 
-        $("#input").val(temp);
-    }
+  // Display input on screen
+  $(".row > button")
+    .unbind()
+    .click(function () {
+      const button = $(this);
+      const buttonCanBeShown =
+        jQuery.inArray(button.val(), notShowableButtons) == -1;
 
-    const square = () => {
-        if ($("#input").val() !== "") {
-            let temp = $("#input").val();
-            temp = eval(temp)
-            temp = Math.pow(temp, SQUARE)
-      
-            $("#input").val(temp);
+      if (buttonCanBeShown) {
+        let lastIndex = button.val();
+        let penultimateIndex = characterOnIndex(1);
+
+        const valueIsSignAndInputIsEmpty =
+          canPressSign.includes(lastIndex) && $("#input").val === "";
+        const lastIndexIsSign =
+          canPressSign.includes(lastIndex) &
+          canPressSign.includes(penultimateIndex);
+
+        if (lastIndexIsSign) {
+          deleteKey();
         }
+        if (!valueIsSignAndInputIsEmpty) {
+          let temp = $("#input").val();
+          temp += lastIndex;
+          $("#input").val(temp);
+        }
+      }
+    });
+
+  $("#clearButton, #clearButton2").click(clearDisplay);
+  $("#deleteButton").click(deleteKey);
+  $("#reverseSignButton").click(reverseSign);
+  $("#squareButton").click(square);
+  $("#squareRootButton").click(squareRoot);
+  $("#inverseButton").click(reciprocate);
+  $("#percentButton").click(calculatePercetage);
+
+  const placeholderButtons =
+    "#reverseSignButton, #squareButton, #squareRootButton, #inverseButton";
+
+  $(placeholderButtons).click(function (button) {
+    const valueIsEmpty = $("#input").val() == "";
+
+    if (valueIsEmpty) {
+      $("#input").attr("placeholder", "Zadejte číslo...");
+    }
+  });
+
+  // Checks user input or evaluate
+  $("#enterButton").click(function () {
+    const input = $(this);
+    const lastCharacter = input.val();
+    const lastCharacterIsSign = canPressSign.includes(lastCharacter);
+
+    if ($("#input").val() == "") {
+      $("#input").attr("placeholder", "Zadejte číslo...");
     }
 
-    const squareRoot = () => {
-        if ($("#input").val() !== "") {
-            let temp = $("#input").val();
-            temp = eval(input.value)
-            temp = Math.sqrt(temp, SQUARE)
-
-            $("#input").val(temp);
-        }
-    }
-
-    const reciprocate = () => {
-        if ($("#input").val() !== "") {
-            let temp = $("#input").val();
-            temp = eval(input.value)
-            temp = 1 / temp
-    
-            $("#input").val(temp);
-        }
-    }
-    
-    const calculatePercetage = () => {
-        alert("Pro toto tlačítka nebyla do zatím přidaná funkcionalita")
-    }
-
-    // Display input on screen
-    $(".row > button").unbind().click(function() {
-        const button = $(this);
-        const buttonCanBeShown = (jQuery.inArray(button.val(), notShowableButtons)) == -1
-
-        if (buttonCanBeShown) {
-            let lastIndex = button.val()
-            let penultimateIndex = characterOnIndex(1)
-
-            const valueIsSignAndInputIsEmpty =
-                canPressSign.includes(lastIndex) && $("#input").val === "";
-            const lastIndexIsSign =
-                canPressSign.includes(lastIndex) &
-                canPressSign.includes(penultimateIndex);
-
-            if (lastIndexIsSign) {
-                deleteKey()
-            }
-            if (!valueIsSignAndInputIsEmpty) {
-                let temp = $("#input").val();
-                temp += lastIndex;
-                $("#input").val(temp);
-            }
-        }
-    })
-
-    $("#clearButton, #clearButton2").click(clearDisplay)
-    $("#deleteButton").click(deleteKey)
-    $("#reverseSignButton").click(reverseSign)
-    $("#squareButton").click(square)
-    $("#squareRootButton").click(squareRoot)
-    $("#inverseButton").click(reciprocate)
-    $("#percentButton").click(calculatePercetage)
-
-    const placeholderButtons = (
-        "#reverseSignButton, #squareButton, #squareRootButton, #inverseButton"
-    )
-
-    $(placeholderButtons).click(function(button) {
-        const valueIsEmpty = $("#input").val() == ""
-
-        if (valueIsEmpty) {
-            $("#input").attr("placeholder", "Zadejte číslo...")
-        }
-    })
-    
-    // Checks user input or evaluate
-    $("#enterButton").click(function () {
-        const input = $(this);
-        const lastCharacter = input.val()
-        const lastCharacterIsSign = canPressSign.includes(lastCharacter)
-
-        if ($("#input").val() == '') {
-            $("#input").val("Enter something...")
-        }
-
-        let result = eval($("#input").val())
-        $("input").val(result)
-    })
-})
+    let result = eval($("#input").val());
+    $("input").val(result);
+  });
+});
