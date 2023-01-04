690f94daef38a4efb8cc4cea015ae9180837c34e
Jakub Plecháč <loxtox74@gmail.com>
2023-01-04 18:41:39 +0100

    Generace tabulky

--- static/js/statistic.js
+++ static/js/statistic.js
@@ -1,6 +1,28 @@
 const allButtons = document.querySelectorAll("button");
 const localHost = "http://localhost:5000";
 
+const table = document.querySelector("table");
+const tbody = document.querySelector("table tbody");
+
+const generateTable = (data) => {
+  // Resetovat tabulku
+  tbody.textContent = "";
+  table.style.display = "table";
+
+  Object.keys(data).forEach((key) => {
+    const row = document.createElement("tr");
+    const button = document.createElement("td");
+    const value = document.createElement("td");
+
+    button.textContent = key;
+    value.textContent = data[key];
+
+    row.appendChild(button);
+    row.appendChild(value);
+    tbody.appendChild(row);
+  });
+};
+
 allButtons.forEach((button) => {
   button.addEventListener("click", async () => {
     const clickedButton = button.textContent;
@@ -16,6 +38,6 @@ allButtons.forEach((button) => {
     });
 
     const data = await response.json();
-    console.log(data);
+    generateTable(data);
   });
 });
